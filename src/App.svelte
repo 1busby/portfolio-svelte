a2c26621d55b9b883a0b96b6f6236b533eb3d33a
Alexander Busby <busbyBAM@gmail.com>
2023-06-11 09:11:40 -0400

    add skills page

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@
 	import Projects from './Projects.svelte';
 	import About from './About.svelte';
 	import Contact from './Contact.svelte';
+	import Skills from './Skills.svelte';
 </script>
 
 <main>
 	<Menu />
 	<About />
+	<Skills />
 	<Projects />
 	<Contact />
 </main>
---
+++ src/Skills.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<main>
+	<p>These are my skills</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
